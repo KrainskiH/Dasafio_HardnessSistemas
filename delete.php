db12909bb68cb8d6173a00134d911164ccdb4a58
Krainski <henriquekrainski38@gmail.com>
2025-06-22 23:02:01 -0300

    Atualizar o delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'config/conexao.php';
 
-// Verificar se o ID foi passado pela URL
 if (!isset($_GET['id']) || empty($_GET['id'])) {
     header("Location: index.php");
     exit;
@@ -9,7 +8,6 @@ if (!isset($_GET['id']) || empty($_GET['id'])) {
 
 $id = $_GET['id'];
 
-// Usar prepared statements
 $stmt = $conexao->prepare("DELETE FROM clientes WHERE id = ?");
 $stmt->bind_param("i", $id);
 
